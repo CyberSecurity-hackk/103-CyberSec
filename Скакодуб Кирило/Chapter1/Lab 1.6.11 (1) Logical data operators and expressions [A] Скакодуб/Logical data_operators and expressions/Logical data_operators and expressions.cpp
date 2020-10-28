ff8c1b06732e25edd8c6f3b5fd99e4b9455b850f
CyberSecurity-hackk <73607723+CyberSecurity-hackk@users.noreply.github.com>
2020-10-28 20:21:36 +0200

    Add files via upload

---
+++ Скакодуб Кирило/Chapter1/Lab 1.6.11 (1) Logical data operators and expressions [A] Скакодуб/Logical data_operators and expressions/Logical data_operators and expressions.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+using namespace std;
+int main(void) {
+	bool answer;
+	int v;
+
+	cout << "Enter a value: ";
+	cin >> v;
+	answer = v >= 0, v < 10, (v * 2) < 20, (v - 2) > -2, (v - 1)>1, (v / 2) > 10, v = 111;
+		cout << (answer ? "THAT'S TRUE :)" : "THAT'S NOT TRUE :(") << endl;
+	cout << "Enter a value: ";
+
+
+	cin >> v;
+	answer = v >= 0, v < 10, (v * 2) < 20, (v - 2) > -2, (v - 1)>1, (v / 2) > 10, v = 111;
+		cout << (answer ? "THAT'S TRUE :)" : "THAT'S NOT TRUE :(") << endl;
+	cout << "Enter a value: ";
+
+
+	cin >> v;
+	answer = v >= 0, v < 10, (v * 2) < 20, (v - 2) > -2, (v - 1)>1, (v / 2) > 10, v = 111;
+		cout << (answer ? "THAT'S TRUE :)" : "THAT'S NOT TRUE :(") << endl;
+	cout << "Enter a value: ";
+
+
+	cin >> v;
+	answer = v >= 0, v < 10, (v * 2) < 20, (v - 2) > -2, (v - 1)>1, (v / 2) > 10, v = 111;
+		cout << (answer ? "THAT'S TRUE :)" : "THAT'S NOT TRUE :(") << endl;
+			cout << "Enter a value: ";
+
+
+	cin >> v;
+	answer = v >= 0, v < 10, (v * 2) < 20, (v - 2) > -2, (v - 1)>1, (v / 2) > 10, v = 111;
+		cout << (answer ? "THAT'S TRUE :)" : "THAT'S NOT TRUE :(") << endl;
+
+
+	cout << "Enter a value: ";
+	cin >> v;
+	answer = v >= 0, v < 10, (v * 2) < 20, (v - 2) > -2, (v - 1)>1, (v / 2) > 10, v = 111;
+		cout << (answer ? "THAT'S TRUE :)" : "THAT'S NOT TRUE :(") << endl;
+
+
+	cout << "Enter a value: ";
+	cin >> v;
+	answer = v >= 0, v < 10, (v * 2) < 20, (v - 2) > -2, (v - 1)>1, (v / 2) > 10, v = 111;
+		cout << (answer ? "THAT'S TRUE :)" : "THAT'S NOT TRUE :(") << endl;
+	return 0;
+}
