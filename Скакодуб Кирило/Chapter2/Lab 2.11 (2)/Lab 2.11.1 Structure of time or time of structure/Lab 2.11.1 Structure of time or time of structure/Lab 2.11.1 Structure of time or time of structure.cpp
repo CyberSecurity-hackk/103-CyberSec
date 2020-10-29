77d7c2bf4fb3bd7929679e9ed99c6c9d1463433b
CyberSecurity-hackk <73607723+CyberSecurity-hackk@users.noreply.github.com>
2020-10-29 10:52:30 +0200

    Add files via upload

---
+++ Скакодуб Кирило/Chapter2/Lab 2.11 (2)/Lab 2.11.1 Structure of time or time of structure/Lab 2.11.1 Structure of time or time of structure/Lab 2.11.1 Structure of time or time of structure.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int h, m;
+    TryAgain:
+    cout << "Enter hours: ";
+    cin >> h;
+   
+    cout << "Enter minutes: ";
+    cin >> m;
+
+    if (h > 24) {
+        cout << "|E_R_R_O_R|";
+        cout << endl;
+        cout << endl;
+        goto TryAgain;
+    }
+    else if (m > 60) {
+        cout << "|E_R_R_O_R|";
+        cout << endl;
+        cout << endl;
+        goto TryAgain;
+    }
+    else {
+        cout << h << ":" << m;
+    }
+    cout << endl;
+    cout << endl;
+    system("pause");
+    return 0;
+}
---
+++ Скакодуб Кирило/Chapter2/Lab 2.11 (2)/Lab 2.11.2 Times and durations/Lab 2.11.2 Times and durations/Lab 2.11.2 Times and durations.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int h, m, h1, m1, hh, mm;
+TryAgain:
+    cout << "Enter begin hours: ";
+    cin >> h;
+
+    cout << "Enter begin minutes: ";
+    cin >> m;
+
+    if (h > 24) {
+        cout << "|E_R_R_O_R|";
+        cout << endl;
+        cout << endl;
+        goto TryAgain;
+    }
+    else if (m > 60) {
+        cout << "|E_R_R_O_R|";
+        cout << endl;
+        cout << endl;
+        goto TryAgain;
+    }
+    else {
+
+        cout << "Enter end hours: ";
+        cin >> h1;
+
+        cout << "Enter end minutes: ";
+        cin >> m1;
+
+        if (h > 24) {
+            cout << "|E_R_R_O_R|";
+            cout << endl;
+            cout << endl;
+            goto TryAgain;
+        }
+        else if (m > 60) {
+            cout << "|E_R_R_O_R|";
+            cout << endl;
+            cout << endl;
+            goto TryAgain;
+        }
+        else {
+            hh = h - h1;
+            mm = m - m1;
+            cout << hh << ":" << mm;
+        }
+    }
+    cout << endl;
+    cout << endl;
+    system("pause");
+    return 0;
+}
