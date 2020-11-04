c1081a0034c1fa5e51a45b441bf5433f8db6a0ca
CyberSecurity-hackk <73607723+CyberSecurity-hackk@users.noreply.github.com>
2020-11-04 11:06:11 +0200

    Add files via upload

---
+++ Коханевич Денис/chapter 3/3.10(1).cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+int main(void) {
+int maxball;
+int ballsno;
+cout << "Max ball number? ";
+cin >> maxball;
+cout << "How many balls? ";
+cin >> ballsno;
+srand(time(NULL));
+int *arr = new int[ballsno];
+bool has;
+for(int i = 0;i<ballsno;i++){
+	do
+	{
+		has = true;
+		arr[i] = rand() % maxball + 1;
+		for(int j = i-1;j>=0; j--)
+			if(arr[j]==arr[i]){
+				has=false;
+				break;
+			}
+	}
+	while(!has);
+
+}
+for(int i = 0; i<ballsno; i++){
+	cout<<arr[i]<<" ";
+}
+delete[] arr;
+return 0;
+}
